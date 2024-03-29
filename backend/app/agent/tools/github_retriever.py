f42b783d6180a2d378ec0e31f6be7c4cfba274a0
Victor Mimo <victormimoc@gmail.com>
2024-03-29 07:13:44 -0300

    cleanup

--- backend/app/agent/tools/github_retriever.py
+++ backend/app/agent/tools/github_retriever.py
@@ -4,14 +4,9 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.tools.retriever import create_retriever_tool
-from app.agent.loader import load_commits
+from app.agent.loader import docs
 
 embeddings = OpenAIEmbeddings()
-
-# tap_docs = load_tap_files()
-docs = load_commits()
-
-print("docs ", docs)
 
 text_splitter = RecursiveCharacterTextSplitter()
 
